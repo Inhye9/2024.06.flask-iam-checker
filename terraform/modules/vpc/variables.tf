c9e86174480fe7f3d2212051b34ad2b95568d080
Csant <41668978+Inhye9@users.noreply.github.com>
2024-06-15 23:16:46 +0900

    2024.06.15. add my_pc_ip

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -7,3 +7,8 @@ variable "name" {
   description = "The name of services"
   type        = string
 }
+
+variable "my_pc_ip" {
+  description = "IP to access to EC2 instance"
+  type        = string
+}
